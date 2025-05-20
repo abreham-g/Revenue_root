d6770f6520477d6fe60f2671a6ad7f58d8c1c4d6
abreham-g <aynuyeabresh@gmail.com>
2025-05-20 05:08:27 +0300

    feat:backend change completed

--- server/auth-service/src/services/manualoa_oxylabsSearch.js
+++ server/auth-service/src/services/manualoa_oxylabsSearch.js
@@ -14,12 +14,25 @@ async function fetchTop10ProductsFromOxylabs(username, password, domain, query) 
     parse: true
   };
 
-  const excludedMerchants = [
-    "u-buy.co.uk", "ebay.com", "alibaba.com", "aliexpress.com", "ebay.co.uk", "wish.com",
-    "asda.com", "sainsburys.co.uk", "tesco.com", "luxplus.co.uk", "lookfantastic.com",
-    "etsy.com", "shein.co.uk", "onbuy.com", "temu.com", "ebay.fr", "fruugo.co.uk",
-    "myuniquebasket.com", "pinterest.com", "walmart.com", "amazon.com", "orientaltrading.com"
-  ];
+ const excludedMerchants = [
+  "u-buy.co.uk", "ebay.com", "alibaba.com", "aliexpress.com", "ebay.co.uk", "wish.com",
+  "asda.com", "sainsburys.co.uk", "tesco.com", "luxplus.co.uk", "lookfantastic.com",
+  "etsy.com", "shein.co.uk", "onbuy.com", "temu.com", "ebay.fr", "fruugo.co.uk",
+  "myuniquebasket.com", "pinterest.com", "walmart.com", "amazon.com", "orientaltrading.com",
+  "newegg.com", "overstock.com", "bonanza.com", "ecrater.com", "poshmark.com", "depop.com",
+  "reverb.com", "chairish.com", "offerup.com", "marketplace.asos.com", "notonthehighstreet.com",
+  "gumtree.com", "preloved.co.uk", "vinted.co.uk", "cex.co.uk", "cdiscount.com", "fnac.com",
+  "rakuten.fr", "leboncoin.fr", "vinted.fr", "manomano.fr", "rueducommerce.fr", "showroomprive.com",
+  "mercari.com", "zozo.jp", "flipkart.com", "snapdeal.com", "myntra.com", "meesho.com",
+  "paytmmall.com", "indiamart.com", "shopclues.com", "limeroad.com", "quikr.com", "olx.in",
+  "tatacliq.com", "subito.it", "privalia.it", "spartoo.it", "yoox.com", "kijiji.it", "eprice.it",
+  "elcorteingles.es", "wallapop.com", "todocoleccion.net", "milanuncios.com", "vinted.es",
+  "rakuten.es", "ebay.de", "otto.de", "zalando.de", "kaufland.de", "kleinanzeigen.de",
+  "hood.de", "quoka.de", "yatego.com", "avocado-store.de", "rakuten.co.jp",
+  "shopping.yahoo.co.jp", "auctions.yahoo.co.jp", "qoo10.jp", "paypaymall.yahoo.co.jp",
+  "ebay.ca", "walmart.ca", "bestbuy.ca", "kijiji.ca", "newegg.ca", "zalando.it",
+  "aliexpress.com", "etsy.com"
+];
 
   try {
     const response = await fetch(url, {
