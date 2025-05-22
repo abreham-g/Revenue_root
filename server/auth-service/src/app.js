76242a0299466b01a0570b4ef6811bb1f181752b
abreham-g <aynuyeabresh@gmail.com>
2025-05-22 19:42:35 +0300

    feat:UI issue fixed and pushed

--- server/auth-service/src/app.js
+++ server/auth-service/src/app.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
     'http://localhost:5173',  
     'http://localhost:5000/api/submit-asins',
     'https://revenue-root-1.onrender.com/api/submit-asins',       
-    'https://revenue-analysis-mwlkvndfc-abreham-gs-projects.vercel.app'
+    'https://revenue-analysis-bswme2715-abreham-gs-projects.vercel.app'
 ];
 app.use(cors({
     origin: function (origin, callback) {
